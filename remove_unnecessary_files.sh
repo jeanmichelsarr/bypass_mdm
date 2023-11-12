17ea42c3bcbcf348274b563b896fa325e9a9d98c
jeanmichelsarr <130646245+jeanmichelsarr@users.noreply.github.com>
2023-11-12 14:34:29 +0000

    Create remove_unnecessary_files.sh
    deactivate some config files

---
+++ remove_unnecessary_files.sh
@@ -0,0 +1,4 @@
+sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+sudo rm /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+sudo touch /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
